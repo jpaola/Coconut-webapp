71975c2e637750d61ae1920365c6a411f2d3df13
Paola Jiron <jpaolababy@gmail.com>
2018-01-12 19:05:31 -0500

    Formatting the time remaining

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,24 @@
 var currentlyPlaylist = [];
 var audioElement;
 
+function formatTime(seconds) {
+    var time = Math.round(seconds);
+    var minutes = Math.floor(time / 60); // convert to minutes and rounds down
+    var seconds = time -(minutes * 60);
+
+    var extraZero = (seconds < 10) ? "0" : "";
+    
+    return minutes + ":" + extraZero + seconds;
+}
+
 function Audio() {
     this.currentlyPlaying;
     this.audio = document.createElement('audio');
 
     this.audio.addEventListener("canplay", function(){
         // 'this' refers to the object that the event was called on
-        $(".progressTime.remaining").text(this.duration);
+        var duration = formatTime(this.duration);
+        $(".progressTime.remaining").text(duration);
     });
 
     this.setTrack = function(track){
