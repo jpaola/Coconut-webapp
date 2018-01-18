ea040c3ea5554b992aba013e04597f0785ce3eef
Paola Jiron <jpaolababy@gmail.com>
2018-01-17 22:30:37 -0500

    Added focus on search. There is a bug on the onfocus (it is not working properly)

--- search.php
+++ search.php
@@ -10,5 +10,23 @@
 
 <div class="searchContainer">
     <h4>Search for an artist, album or song</h4>
-    <input type="text" class="searchInput" value="<?php echo $term; ?>" placeholder="Search.. ">
+    <input type="text" class="searchInput" value="<?php echo $term; ?>" placeholder="Search..." onfocus="this.value = this.value">
 </div>
+
+<script>
+
+    $(".searchInput").focus();
+
+    $(function(){
+        var timer;
+
+        $(".searchInput").keyup(function() {
+            clearTimeout(timer); // clear timer each time user stops typing
+
+            timer = setTimeout(function() {
+                var val = $(".searchInput").val(); // save the user input
+                openPage("search.php?term=" + val);
+            }, 1000); // 1 second
+        })
+    })
+</script>
